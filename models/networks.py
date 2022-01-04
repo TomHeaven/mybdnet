fc9082962d333ffcacbd25fd9ac85a8fea02d9ea
TomHeaven <TomHeaven@qq.com>
2022-01-04 17:57:00 +0800

    running 3-frames

---
+++ models/networks.py
@@ -0,0 +1,20 @@
+import torch
+import models.archs.BDNet as BDNet
+
+# Generator
+def define_G(opt):
+    opt_net = opt['network_G']
+    which_model = opt_net['which_model_G']
+
+    # image restoration
+
+    if which_model == 'BDNet':
+        print('opt_net[\'nframes\']', opt_net['nframes'])
+        netG = BDNet.BDNet_train(nf=opt_net['nf'], nframes=opt_net['nframes'],
+                              groups=opt_net['groups'], front_RBs=opt_net['front_RBs'],
+                              back_RBs=opt_net['back_RBs'], center=opt_net['center'])
+    else:
+        raise NotImplementedError('model [{:s}] not recognized'.format(which_model))
+    return netG
+
+
---
+++ run.sh
@@ -0,0 +1,1 @@
+nohup python3 train_BDNet_real_static.py > run.log 2>&1 &
