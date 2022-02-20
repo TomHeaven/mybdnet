5ee2e76bd912577dbdd46ae4b85f44c7c5afed51
TomHeaven <tomheaven@qq.com>
2022-02-20 13:56:04 +0800

    add sim data

--- data/video_denoising_test_dataset.py
+++ data/video_denoising_test_dataset.py
@@ -248,3 +248,76 @@ class My_real(data.Dataset):
     def __len__(self):
         return len(self.subfolders_GT)
 
+class My_sim(data.Dataset):
+    def __init__(self, opt):
+        super(My_sim, self).__init__()
+        self.opt = opt
+        self.half_N_frames = opt['N_frames'] // 2
+        # GS: No need LR
+        #self.GT_root, self.LQ_root = opt['dataroot_GT'], opt['dataroot_LQ']
+        self.GT_root = opt['dataroot_GT']
+        self.data_info = {'path_LQ': [], 'path_GT': [], 'folder': []}   ## They are flattened
+        #### Generate data info and cache data
+        
+        # subfolders_LQ = util.glob_file_list(self.LQ_root)
+        self.subfolders_GT = sorted(glob.glob(self.GT_root+'/*'))
+    
+    def fix_bayer_size(self, bayer):
+        h, w = bayer.shape
+        new_h = h - h%8
+        new_w = w - w%8
+        bayer = bayer[:new_h,:new_w]
+        return bayer
+        
+    def __getitem__(self, index):  ## The index indicates the frame index which is flattened
+        #GT_size = 384
+        folder_path = self.subfolders_GT[index]
+        img_paths = sorted(glob.glob(folder_path+'/*.tiff'))[0:self.opt['N_frames']]
+        rggb_l = []
+        noise_l = []
+        sigma = 5 / 255.0
+        for img_path in img_paths:
+            if DEBUG:
+                print('img_path', img_path)
+            bayer = cv2.imread(img_path, cv2.IMREAD_UNCHANGED) / 255.0
+            if DEBUG:
+                print('bayer', bayer.shape)
+            bayer = self.fix_bayer_size(bayer)
+            if DEBUG:
+                print('fixed bayer', bayer.shape)
+            rggb = bayer2bayer3d(bayer, 'grbg')
+            noise = np.ones(rggb.shape[:2]) * sigma
+            noise_l.append(noise)
+            if self.opt['pre_demosaic'] == True:
+                rggb = util.demosaic(rggb)
+            rggb_l.append(rggb)
+            
+        rggb_case = np.stack(rggb_l,0)    
+        rggb_in = np.transpose(rggb_case,(0,3,1,2)) # N,C,H,W
+        
+        # Fake gt_data
+        gt_data = rggb
+        gt = gt_data
+        gt = np.transpose(gt,(2,0,1))
+         
+        noise = np.stack(noise_l,2) 
+        noise_map = np.mean(noise)
+        noise_map = np.tile(noise_map.reshape(1,1,1,1),[1,1,rggb.shape[0],rggb.shape[1]]) if self.opt['pre_demosaic'] == False else np.tile(noise_map.reshape(1,1,1,1),[1,1,rggb.shape[0]*2,rggb.shape[1]*2])
+        #LQ_size_tuple = (4,512,512)
+        rggb_in = rggb_in[:,:,0:rggb.shape[0], 0:rggb.shape[1]] if self.opt['pre_demosaic'] == False else rggb_in[:,:,0:rggb.shape[0]*2, 0:rggb.shape[1]*2]
+        noise_in_unpro = noise_map
+        #gt = gt[:,0:GT_size*2,0:GT_size*2]
+        if DEBUG:
+            print('noise', noise.shape, 'noise_l', noise_l[0].shape, 'rggb_in', rggb_in.shape)
+            print('noise_map', noise_map.shape)
+         
+        img_LQs = torch.from_numpy(np.ascontiguousarray(rggb_in)).float()
+        noise_in = torch.from_numpy(np.ascontiguousarray(noise_map)).float()
+        gt = torch.from_numpy(np.ascontiguousarray(gt)).float()
+        noise_in_unpro = torch.from_numpy(np.ascontiguousarray(noise_in_unpro)).float()
+
+        return {'LQs':img_LQs,'noise_in':noise_in,'GT':gt,'folder':folder_path,'noise_in_unpro':noise_in_unpro}
+
+    def __len__(self):
+        return len(self.subfolders_GT)
+
